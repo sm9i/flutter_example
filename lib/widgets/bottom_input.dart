75f8e9999f268d6636b44698cd856469c91084a4
sm9i <renzhayi@88.com>
2021-08-09 19:16:47 +0800

    test

--- lib/widgets/bottom_input.dart
+++ lib/widgets/bottom_input.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void showBottomInput(BuildContext context) {
-  MethodChannel(name).invokeMethod(method)
   showModalBottomSheet(
     context: context,
     builder: (context) {
